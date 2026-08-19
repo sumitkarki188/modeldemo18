163b7799c88eeeca914344f369486bf338ced9a9
Sumit Singh Karki <sumitkarki1114@gmail.com>
2026-08-19 19:05:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
-  },
-
-  vite: {
-    // additional Vite configuration
+    server: {
+      entry: "server",
+    },
   },
 
   nitro: {
